75e07868310a9031205327b2c3fcc5b110227185
EvanMworia <evannji99@gmail.com>
2026-01-29 10:50:03 +0300

    Initial Commit

---
+++ 00-typescript/day-2/basics.ts
@@ -0,0 +1,9 @@
+//correctly type variables for a scanId (number), targetUrl (string), and isCompleted (boolean).
+let scanId: number;
+let targetUrl: string;
+let isCompleted: boolean;
+// Assign appropriate values to each variable
+scanId = 12345;
+targetUrl = 'https://example.com/scan';
+isCompleted = false;
+console.log(`Scan ID: ${scanId}, Target URL: ${targetUrl}, Is Completed: ${isCompleted}`);
